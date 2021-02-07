f56bada06f561f1dba177538f1b79c626d1471ab
Abhinav Raut <abhinavrautcs@gmail.com>
2021-02-08 02:49:31 +0530

    added dotenv

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
-
 const app = express();
+require('dotenv').config();
 
 app.get("/", (req, res) => {
     res.send("Hello world!");
 });
 
-app.listen(80);
+const server = app.listen(process.env.PORT);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET"]
+    }
+});
+
+io.on('connection', socket => {
+    console.log("User connected with id " + socket.id);
+});
+
