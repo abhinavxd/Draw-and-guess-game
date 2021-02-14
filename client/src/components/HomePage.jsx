33f8b299887ab7569c5ff3bc85560b1fcc628b63
Abhinav Raut <abhinavrautcs@gmail.com>
2021-02-14 18:31:25 +0530

    align menu to center and added more css

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,16 +1,17 @@
+import "../css/homePage.css";
 const HomePage = (props) => {
     const startGameHandler = () => {
         props.setGameStarted(true);
     };
     return (
-        <div>
+        <div id="menu-container">
             <h1>Welcome to glowing meme</h1>
             <div>
                 <input placeholder='Enter your name'></input>
             </div>
             <br></br>
             <div>
-                <button onClick={startGameHandler}>Play</button>
+                <button className='play-button' onClick={startGameHandler}>Play</button>
             </div>
         </div>
     );
