82135fc96e5897374c5201c6b0308b3b786c7249
Abhinav Raut <abhinavrautcs@gmail.com>
2021-02-17 03:03:24 +0530

    Topgame header component

---
+++ client/src/components/TopGameHeader.jsx
@@ -0,0 +1,12 @@
+import "../css/topGameHeader.css";
+const TopGameHeader = () => {
+    return (
+        <div class='top-bar-game'>
+            <div id="current-word">
+                _ _ _ _ _ _
+            </div>
+        </div>
+    );
+};
+
+export default TopGameHeader;
