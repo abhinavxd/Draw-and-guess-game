9b44f0b84b43ad767b6b7ac26612e6aa015bb86f
Abhinav Raut <abhinavrautcs@gmail.com>
2021-02-17 03:02:19 +0530

    move state into homepage component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 import { useState } from "react";
 import HomePage from './components/HomePage';
 import "./App.css";
-import PlayArea from "./components/PlayArea";
 function App() {
-  const [gameStarted, setGameStarted] = useState(false);
   return (
     <div className="App">
-      {!gameStarted ? <HomePage setGameStarted={setGameStarted}></HomePage> : <PlayArea></PlayArea>}
+      <HomePage />
     </div>
   );
 }
