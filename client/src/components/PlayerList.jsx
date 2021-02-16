80c431554c4c48fe9d71206032bcd58c343b5fa8
Abhinav Raut <abhinavrautcs@gmail.com>
2021-02-17 03:03:01 +0530

    side menu for playerlist

---
+++ client/src/components/PlayerList.jsx
@@ -0,0 +1,18 @@
+import { Fragment } from "react";
+import "../css/playerList.css";
+const PlayerList = () => {
+    return (
+        <Fragment>
+            <div class='player'>
+                <div>
+                    Abhinav
+                </div>
+                <div>
+                    Points: 599
+                </div>
+            </div>
+        </Fragment>
+    )
+};
+
+export default PlayerList;
