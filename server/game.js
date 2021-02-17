4334a466a06429a0eb668610f594a0bf776fdcf8
Abhinav Raut <abhinavrautcs@gmail.com>
2021-02-18 00:26:40 +0530

    move socket game logic to another file

---
+++ server/game.js
@@ -0,0 +1,25 @@
+const io, gameSocket;
+const initGame = (io_, socket) => {
+    io = io_;
+    gameSocket = socket;
+
+    // server events
+    gameSocket.on('createNewGame', hostCreateNewGame);
+    // gameSocket.on('hostRoomFull', hostCreateNewGame);
+    // gameSocket.on('hostCountDownFinished', hostCreateNewGame);
+    // gameSocket.on('hostNextRound', hostCreateNewGame);
+
+    gameSocket.on('cords', handlePlayerCoordinates);
+    gameSocket.on('new_message', handleNewMessage);
+    gameSocket.on('erase', handlePlayerEraseAction);
+}
+
+const handlePlayerCoordinates = (data) => {
+    gameSocket.broadcast.emit('cords', { x: data.x, y: data.y, prevX: data.prevX, prevY: data.prevY });
+}
+
+const handleNewMessage = (data) => {
+    io.sockets.emit('new_message', { msg: data.msg });
+};
+
+module.exports.initGame;
