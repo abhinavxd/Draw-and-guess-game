77696ee474c6e49212b3a4407f5a03bb4158151e
Abhinav raut <abhinavrautcs@gmail.com>
2021-02-17 19:10:23 +0000

    remove comments

--- server/index.js
+++ server/index.js
@@ -29,13 +29,5 @@ const io = require('socket.io')(server, {
 io.on('connection', socket => {
     console.log("User connected with id " + socket.id);
     game.initGame(io, socket);
-    // socket.on('cords', (data) => {
-    //     socket.broadcast.emit('cords', { x: data.x, y: data.y, prevX: data.prevX, prevY: data.prevY });
-    // });
-    // socket.on('erase', (data) => {
-    //     socket.emit('erase');
-    // });
-    // socket.on('new_message', (data) => {
-    //     io.sockets.emit('new_message', { msg: data.msg });
-    // });
+
 });
