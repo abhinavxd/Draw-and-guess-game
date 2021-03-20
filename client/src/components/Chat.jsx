7d7e065f52d2334ebcf766ac90a8b371af4cbb75
Abhinav Raut <abhinavrautcs@gmail.com>
2021-03-21 03:04:21 +0530

    lift the state to parent component for chat

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,49 +1,35 @@
 import "./../css/chat.css";
-import { Fragment, useState, useRef, useEffect } from "react";
+import { Fragment, useEffect, useRef } from "react";
 const Chat = (props) => {
-    let inputField = useRef(null);
 
-    const [chatMessages, setChatMessages] = useState([]);
-
-    const [inputBarText, setInputBarText] = useState("");
-
-    const handleNewMessage = (e) => {
-        e.preventDefault();
-        props.soc.current.emit('new_message', { 'msg': inputBarText });
-        setInputBarText("");
+    const chatScrollDiv = useRef();
+    const scrollToBottom = () => {
+        if (chatScrollDiv.current) {
+            console.log('scroll down!!');
+            chatScrollDiv.current.scrollTo(
+                0,
+                chatScrollDiv.current.scrollHeight
+            );
+        }
     };
 
     useEffect(() => {
-        if (props.soc.current) {
-            props.soc.current.on('new_message', (data) => {
-                setChatMessages((prevState) => {
-                    let newState = [...prevState];
-                    let inputMessage = data.msg;
-                    newState.push(inputMessage);
-                    return newState;
-                });
-            });
-        }
-
-    }, [props.soc.current])
-
-
-    const handleChangeMessage = (e) => {
-        setInputBarText(e.target.value)
-    }
+        scrollToBottom();
+    }, [props.chatMessages]);
 
     return (
         <Fragment>
-            <div className="chats">
-                {chatMessages.length > 0 ? (chatMessages.map((item) => (
-                    <p key={Math.floor(Math.random() * 1000) + 1}>{item}</p>
+            <div className="chats" ref={chatScrollDiv}>
+                {props.chatMessages.length > 0 ? (props.chatMessages.map((item, index) => (
+                    <p key={index}>{item}</p>
                 ))) :
                     ("")
                 }
             </div>
-            <form id="chatInput" onSubmit={handleNewMessage}>
-                <input ref={inputField} onChange={handleChangeMessage} value={inputBarText} className="form-control" id="inputChat" autoComplete="off" type="text" placeholder="Type your guess here.." maxLength="100" />
+            <form id="chatInput" onSubmit={props.handleNewMessage}>
+                <input onChange={props.handleChangeMessage} value={props.inputBarText} className="form-control" id="inputChat" autoComplete="off" type="text" placeholder="Type your guess here.." maxLength="100" />
             </form>
+            <div ></div>
         </Fragment>
     )
 }
