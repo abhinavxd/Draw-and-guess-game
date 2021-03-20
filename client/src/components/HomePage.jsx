4b9da079b0d54006665834c74145fb72f5c66a8d
Abhinav Raut <abhinavrautcs@gmail.com>
2021-03-21 03:05:16 +0530

    listen to gam over, roomid, new msg client list and other events

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,11 +1,21 @@
 import "../css/homePage.css";
 import Header from "./Header";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import PlayArea from "./PlayArea";
 
 const HomePage = () => {
     const [gameStarted, setGameStarted] = useState(false);
+    const [roomId, setGameRoomId] = useState(undefined);
+    const [playerName, setCurrPlayerName] = useState(undefined);
+    const [action, setAction] = useState('create');
+    const playerRoomId = useRef(undefined)
+    const playerNameRef = useRef(undefined);
     const startGameHandler = () => {
+        if (playerRoomId.current.value) {
+            setGameRoomId(playerRoomId.current.value);
+            setAction('join')
+        }
+        setCurrPlayerName(playerNameRef.current.value);
         setGameStarted(true);
     };
     return (
@@ -17,11 +27,14 @@ const HomePage = () => {
                         <h1>Welcome!</h1>
                     </div>
                     <div>
-                        <input placeholder='Enter your name'></input>
+                        <input type="text" placeholder='Enter your name' ref={playerNameRef}></input>
+                    </div>
+                    <div>
+                        <input type="text" placeholder='Enter your room id' name="room-id" ref={playerRoomId} id="player-room-id" />
                     </div>
                     <br></br>
                     <button type='button' className='btn btn-success' onClick={startGameHandler}>Play</button>
-                </div> : <PlayArea />}
+                </div> : <PlayArea roomId={roomId} playerName={playerName} action={action} />}
         </div>
     );
 }
--- client/src/components/PlayArea.jsx
+++ client/src/components/PlayArea.jsx
@@ -1,13 +1,26 @@
 import { io } from 'socket.io-client';
-import { useCallback, useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import "../css/playArea.css";
 import Chat from "./Chat";
 import PlayerList from './PlayerList';
 import TopGameHeader from './TopGameHeader';
 
-const PlayArea = () => {
+const PlayArea = (props) => {
+    const [curGameId, setGameId] = useState(undefined);
+    const [currentWord, setCurrentWord] = useState(undefined);
+    const [showNewWordOverlay, setshowNewWordOverlay] = useState(false);
+    const [playersList, setPlayersList] = useState([]);
+    const [scoreBoard, setScoreBoard] = useState([]);
+    const [showScoreBoard, setShowScoreBoard] = useState(false);
+    const [isGameOver, setGameOver] = useState(false);
+
+    // Chat state
+    const [chatMessages, setChatMessages] = useState([]);
+    const [inputBarText, setInputBarText] = useState("");
+
 
     let soc = useRef(null);
+    let isCurrentPlayersTurn = useRef(false);
     let w = useRef(800);
     let h = useRef(600);
     let canvas = useRef();
@@ -23,7 +36,11 @@ const PlayArea = () => {
     let y = useRef(3);
 
 
+
     const findxy = useCallback((res, e) => {
+        if (!isCurrentPlayersTurn.current) {
+            return;
+        }
         if (res === 'down') {
             prevX.current = currX.current;
             prevY.current = currY.current;
@@ -73,38 +90,7 @@ const PlayArea = () => {
         }, false);
     }, [findxy]);
 
-    const color = (obj) => {
-        switch (obj.id) {
-            case "green":
-                x = "green";
-                break;
-            case "blue":
-                x = "blue";
-                break;
-            case "red":
-                x = "red";
-                break;
-            case "yellow":
-                x = "yellow";
-                break;
-            case "orange":
-                x = "orange";
-                break;
-            case "black":
-                x = "black";
-                break;
-            case "white":
-                x = "white";
-                break;
-            default:
-
-        }
-        if (x === "white") y = 14;
-        else y = 2;
-
-    }
-
-    function draw() {
+    const draw = () => {
         ctx.current.beginPath();
         ctx.current.moveTo(prevX.current, prevY.current);
         ctx.current.lineTo(currX.current, currY.current);
@@ -114,15 +100,40 @@ const PlayArea = () => {
         ctx.current.closePath();
     }
 
-    function erase() {
+    const erase = () => {
         ctx.current.clearRect(0, 0, w.current, h.current);
         soc.current.emit('erase');
     }
 
     useEffect(() => {
         init();
-        soc.current = io(process.env.REACT_APP_API_URL);
-    }, [init]);
+        soc.current = io(process.env.REACT_APP_API_URL, {
+            query: {
+                username: props.playerName,
+                roomId: props.roomId,
+                action: props.action
+            }
+        });
+        soc.current.on('new-word', (data) => {
+            if (soc.current.id === data.to_socket_id) {
+                console.log('My turn!')
+                isCurrentPlayersTurn.current = true;
+            }
+            setCurrentWord(data.word);
+            setShowScoreBoard(false)
+            setshowNewWordOverlay(true)
+        });
+        soc.current.on('current-turn', (data) => {
+            console.log('Current turn ', data.username);
+            isCurrentPlayersTurn.current = false;
+            setChatMessages([]);
+        });
+        soc.current.on('round-over', (data) => {
+            setScoreBoard(data.clients);
+            setShowScoreBoard(true);
+            setshowNewWordOverlay(false);
+        });
+    }, [init, props.playerName, props.roomId, props.action]);
 
     useEffect(() => {
         if (soc.current) {
@@ -136,33 +147,116 @@ const PlayArea = () => {
             soc.current.on('erase', () => {
                 erase();
             });
-
+            soc.current.on('newGameCreated', (data) => {
+                setGameId(data.gameId);
+            });
+            soc.current.on('client-list', (data) => {
+                setPlayersList(data.clients)
+            });
+            soc.current.on('clear-board-and-current-word', (data) => {
+                setCurrentWord(undefined);
+            });
+            soc.current.on('game-over', (data) => {
+                setGameOver(true);
+                setScoreBoard(data.clients);
+            });
+            soc.current.on('room-id', (data) => {
+                setGameId(data.id);
+            });
+            soc.current.on('new_message', (data) => {
+                setChatMessages((prevState) => {
+                    let newState = [...prevState];
+                    let inputMessage = data.msg;
+                    newState.push(inputMessage);
+                    return newState;
+                });
+            });
+            soc.current.on('correct-answer', data => {
+                setChatMessages((prevState) => {
+                    let newState = [...prevState];
+                    let inputMessage = data.username;
+                    inputMessage += " has guessed the word!";
+                    newState.push(inputMessage);
+                    return newState;
+                });
+            });
         }
     }, [soc])
 
     useEffect(() => {
-        if (soc.current) {
-            soc.current.on('cords', (data) => {
-                currX.current = data.x;
-                currY.current = data.y;
-                draw();
+        if (props.roomId && props.roomId.length > 0) {
+            soc.current.emit("playerJoinGame", { gameId: props.roomId })
+            setGameId(props.roomId);
+        } else {
+            soc.current.on('newGameCreated', (data) => {
+                setGameId(data.gameId);
             });
         }
+    }, [props.roomId]);
 
-    }, [soc])
+    const hideOverlay = () => {
+        setshowNewWordOverlay(false)
+        setShowScoreBoard(false)
+    };
+
+    const handleNewMessage = (e) => {
+        e.preventDefault();
+        soc.current.emit('new_message', { 'msg': inputBarText });
+        setInputBarText("");
+    };
+
+
+    const handleChangeMessage = (e) => {
+        setInputBarText(e.target.value)
+    }
 
     return (
         <div className='gameScreen'>
-            <TopGameHeader />
-            <div className="parentContainer">
+            {showNewWordOverlay && <div className='overlay-container'>
+                <div className='overlay-content'>
+                    <i onClick={hideOverlay} className='fas fa-times close-overlay'></i>
+                    <div>
+                        Current Word:
+                    </div>
+                    <div>
+                        {currentWord}
+                    </div>
+                </div>
+            </div>}
+            {showScoreBoard && <div className='overlay-container'>
+                <div className='overlay-content'>
+                    <i onClick={hideOverlay} className='fas fa-times close-overlay'></i>
+                    <div>
+                        <div>
+                            SCORE BOARD
+                        </div>
+                        {scoreBoard.map((client, index) => (
+                            <div key={index}>
+                                {isGameOver ? (
+                                    <div>
+                                        {index + 1} {client.username} : {client.score}
+                                    </div>
+                                ) : (
+                                    <div>
+                                        {client.username} : {client.score}
+                                    </div>
+                                )}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>}
+            <TopGameHeader gameId={curGameId} currentWord={currentWord} />
+            < div className="parentContainer">
                 <div id='playerList'>
-                    <PlayerList />
+                    <PlayerList players={playersList} />
                 </div>
                 <div id='containerCanvas'>
                     <canvas width={800} height={600} id={'can'} />
                 </div>
                 <div id="chatContainer">
-                    <Chat soc={soc} />
+                    <Chat chatMessages={chatMessages} inputBarText={inputBarText} handleNewMessage={handleNewMessage} handleChangeMessage={handleChangeMessage}
+                    />
                 </div>
                 {/* <div className='containerToolBar'>
                     <button onClick={erase}>Erase</button>
