9d44c9b595887578521cd233c48c0e78fd4f32ec
Abhinav Raut <abhinavrautcs@gmail.com>
2021-03-21 03:08:23 +0530

    file to initiate mongodb connection

---
+++ db/database.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const consola = require('consola');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on('connected', () => {
+    consola.success('Mongo db connection establised');
+});
+
+db.on('error', err => {
+    consola.warn(`Mongo db connection error : ${err}`);
+});
+
+db.on('disconnected', () => {
+    consola.warn('Disconnected to mongo db server');
+});
