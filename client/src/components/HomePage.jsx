fed5b5ece3b2bd03e28f49ef564bc416e0c1b037
Abhinav Raut <abhinavrautcs@gmail.com>
2021-04-02 01:58:58 +0530

    add labels to homepage inputs  and add checkbox for hiding and showing input for game room id

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -8,33 +8,56 @@ const HomePage = () => {
     const [roomId, setGameRoomId] = useState(undefined);
     const [playerName, setCurrPlayerName] = useState(undefined);
     const [action, setAction] = useState('create');
+    const [joinRoom, setJoinRoom] = useState(false);
     const playerRoomId = useRef(undefined)
     const playerNameRef = useRef(undefined);
+
+    /**
+     * Handler to start game and socket connection
+     */
     const startGameHandler = () => {
-        if (playerRoomId.current.value) {
+        if (playerRoomId.current && playerRoomId.current.value) {
             setGameRoomId(playerRoomId.current.value);
             setAction('join')
         }
         setCurrPlayerName(playerNameRef.current.value);
         setGameStarted(true);
     };
+
+    /**
+     * Show/hide input for room-id
+     */
+    const showJoinRoomInput = () => {
+        setJoinRoom(!joinRoom);
+    };
     return (
         <div className='container-fluid'>
             <Header />
             {!gameStarted ?
                 <div id="menu-container" >
-                    <div className='color-white'>
-                        <h1>Welcome!</h1>
+                    <div style={{ marginBottom: '0.8em' }}>
+                        <h1>Draw and guess!</h1>
                     </div>
                     <div>
-                        <input type="text" placeholder='Enter your name' ref={playerNameRef}></input>
+                        <label>
+                            Enter your username
+                            <input type="text" required='true' ref={playerNameRef}></input>
+                        </label>
                     </div>
                     <div>
-                        <input type="text" placeholder='Enter your room id' name="room-id" ref={playerRoomId} id="player-room-id" />
+                        <label>
+                            <span style={{ marginRight: 10 }}>Have room-id?</span>
+                            <input style={{ padding: 10 }} onClick={showJoinRoomInput} type='checkbox' ></input>
+                        </label>
                     </div>
-                    <br></br>
+                    {joinRoom && <div>
+                        <label>
+                            Enter your friends room id
+                        <input type="text" name="room-id" ref={playerRoomId} id="player-room-id" />
+                        </label>
+                    </div>}
                     <button type='button' className='btn btn-success' onClick={startGameHandler}>Play</button>
-                    <h5>v1.0.1</h5>
+                    <h5>{process.env.REACT_APP_VER}</h5>
                 </div> : <PlayArea roomId={roomId} playerName={playerName} action={action} />}
         </div>
     );
