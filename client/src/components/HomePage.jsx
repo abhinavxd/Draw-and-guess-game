693df41632010623d5a33608f57e90f0d949b731
sskanishk <kanishkmalviya78@gmail.com>
2021-06-13 23:21:49 +0530

    main page UI done

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -39,33 +39,47 @@ const HomePage = () => {
     return (
         <div className='container'>
             {!gameStarted ?
-                <div>  {/* id="menu-container" */}
+                <div className="mt-5"> 
                     <div style={{ marginBottom: '0.8em' }}>
-                        <h1>Draw and guess!</h1>
+                    <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                        {/* <span class="block xl:inline">Data to enrich your</span> */}
+                        <span class="block text-indigo-600 xl:inline">Draw and guess!s</span>
+                    </h1>
                     </div>
-                    <div>
-                        <label>
-                            Enter your username
-                            <input type="text" required={true} className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md" ref={playerNameRef}></input>
+
+                    <div class="w-full max-w-sm">
+                        <div class="mb-4">
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                            Username
                         </label>
+                        <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" placeholder="jenny" ref={playerNameRef} />
                         {error && <p className='warn'>Please enter username</p>}
-                    </div>
-                    <div>
-                        <label>
-                            <span style={{
-                                position: 'relative', top: -1,
-                                right: 10
-                            }}>Have room-id?</span>
-                            <input style={{ padding: 10 }} onClick={showJoinRoomInput} type='checkbox' ></input>
+                        </div>
+
+                        <div class="mb-4" >
+                        <label class="md:w-2/3 block text-gray-500 font-bold">
+                            <input class="mr-2 leading-tight" type="checkbox" onClick={showJoinRoomInput} />
+                            <span class="text-sm">
+                                Have friend's room-id ?
+                            </span>
+                            </label>
+                        </div>
+
+                        { joinRoom && <div class="mb-4">
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                                Room Id
                         </label>
+                        <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="text" name="room-id" ref={playerRoomId} id="player-room-id" placeholder="asdf53b723rh9bs9d" />
+                        </div> }
+
+                        <div class="mb-4">
+                        <button class="shadow bg-indigo-500 hover:bg-indigo-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button" onClick={startGameHandler} >
+                            Lest Play!
+                        </button>
+                        </div>
+
                     </div>
-                    {joinRoom && <div>
-                        <label>
-                            Enter your friends room id
-                        <input type="text" name="room-id" ref={playerRoomId} id="player-room-id" />
-                        </label>
-                    </div>}
-                    <button type='button' className='btn btn-success' onClick={startGameHandler}>Play</button>
+
                     <div className="homepage-about">
                         <h5>
                             How to play?
