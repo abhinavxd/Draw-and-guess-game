09f3fa5f349ac4085f396914487f44f57a34edb0
Abhinav Raut <abhinavrautcs@gmail.com>
2021-06-27 01:23:10 +0530

    remove unused id from div

--- client/src/components/TopGameHeader.jsx
+++ client/src/components/TopGameHeader.jsx
@@ -3,7 +3,7 @@ const TopGameHeader = ({ gameId, gameStarted, currentWord }) => {
     return (
         <div className={styles.topGameBar}>
             <div>{gameId !== undefined ? (<div>{!gameStarted ? "To start drawing share this URL with your friend" : "Room URL"} : <a href={`${process.env.REACT_APP_DOMAIN}/${gameId}`}>{process.env.REACT_APP_DOMAIN}/{gameId}</a></div>) : ("")} </div>
-            {currentWord && <div id="current-word">
+            {currentWord && <div>
                 Current word <span style={{ letterSpacing: '5px', paddingLeft: 10 }}>{currentWord}</span>
             </div>}
         </div>
