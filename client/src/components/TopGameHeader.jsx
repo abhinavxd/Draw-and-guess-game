091a8a84a4d55a3cb58ddca5ec4361e2f4e76071
Abhinav Raut <abhinavrautcs@gmail.com>
2022-06-12 01:39:22 +0530

    fix: get game url from window obj.

--- client/src/components/TopGameHeader.jsx
+++ client/src/components/TopGameHeader.jsx
@@ -2,7 +2,7 @@ import styles from "../css/topGameHeader.module.css";
 const TopGameHeader = ({ gameId, gameStarted, currentWord }) => {
     return (
         <div className={styles.topGameBar}>
-            <div>{gameId !== undefined ? (<div>{!gameStarted ? "To start drawing share this URL with your friend" : "Room URL"} : <a href={`${process.env.REACT_APP_DOMAIN}/${gameId}`}>{process.env.REACT_APP_DOMAIN}/{gameId}</a></div>) : ("")} </div>
+            <div>{gameId !== undefined ? (<div>{!gameStarted ? "To start drawing share this URL with your friend" : "Room URL"} : <a href={`${window.location.origin}/${gameId}`}>{process.env.REACT_APP_DOMAIN}/{gameId}</a></div>) : ("")} </div>
             {currentWord && <div>
                 Current word <span style={{ letterSpacing: '5px', paddingLeft: 10 }}>{currentWord}</span>
             </div>}
